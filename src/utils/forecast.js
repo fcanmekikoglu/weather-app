76d7bc4309dec7445038c89efae4818f68261a99
Fahrettin Can Mekikoğlu <fcanmekikoglu@gmail.com>
2022-07-30 19:26:35 +0300

    removed unnecessary console.log

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require ('request')
 
 const forecast = (lat, lon, callback) =>{
     const url = 'http://api.weatherstack.com/current?access_key=f241dcff627784ecd10e8f9bf8621ecf&query='+ lon + ',' + lat
-    console.log(url)
+    
     request({url, json:true}, (error, {body})=>{
         if(error){
             callback('Unable to connect weather services', undefined )
